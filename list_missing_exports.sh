5073d2bc170515cf8f47c5de7b4a611fc38204f6
Andrea Stacchiotti <andreastacchiotti@gmail.com>
2022-01-01 17:24:24 +0100

    [HUMAN] The archive is full, no need to list missing exorts anymore

--- list_missing_exports.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for f in $(for s in */*/scores.txt; do
-              < "$s" tr '/' '-' | sed -e "s@^@$(dirname $s)/@g" -e 's@$@.txt@'
-           done); do
-    if [[ ! -f $f ]]; then
-        echo "$f"
-    fi
-done
